5e46c526bd1fcb7e013032766abe7f177c7ba691
sashmit122@gmail.com <sashmit122@gmail.com>
2024-07-26 16:37:36 +0530

    test commit

--- provider.tf
+++ provider.tf
@@ -1,10 +1,8 @@
 provider "snowflake" {
   role = "SYSADMIN"
-  private_key_path = "C:/Users/Elait112.ELAIT-DT-CHE-W-/Permifrost/snowflake_tf_snow_key.p8"
 }
 
 provider "snowflake" {
   alias = "security_admin"
   role  = "SECURITYADMIN"
-  private_key_path = "C:/Users/Elait112.ELAIT-DT-CHE-W-/Permifrost/snowflake_tf_snow_key.p8"
 }
