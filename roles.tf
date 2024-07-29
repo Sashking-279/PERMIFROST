d1ef29a06a819ff92f650c6e6eca7f5b7135b7d1
sashmit122@gmail.com <sashmit122@gmail.com>
2024-07-29 10:44:04 +0530

    Test 2

--- roles.tf
+++ roles.tf
@@ -1,9 +1,9 @@
-resource "snowflake_account_role" "role" {
+resource "snowflake_role" "role" {
   provider = snowflake.security_admin
   name     = "TF_DEMO_SVC_ROLE"
 }
 
-resource "snowflake_account_role" "reporter" {
+resource "snowflake_role" "reporter" {
   name    = "REPORTER"
   provider = snowflake.security_admin
   comment = "This role is limited to read only querying on the reporting database"
