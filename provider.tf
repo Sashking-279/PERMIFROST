cc45a395ff3a41ff22a5b9962bf6c421ee759b0a
sashmit122@gmail.com <sashmit122@gmail.com>
2024-07-29 18:21:04 +0530

    initial test

--- provider.tf
+++ provider.tf
@@ -1,8 +1,11 @@
 provider "snowflake" {
   role = "SYSADMIN"
+  private_key_path = "snowflake_tf_snow_key.p8"
 }
 
 provider "snowflake" {
   alias = "security_admin"
   role  = "SECURITYADMIN"
+  private_key_path = "snowflake_tf_snow_key.p8"
+
 }
