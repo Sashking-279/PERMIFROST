d5e094ba88956e775658c68841850162ac52646b
sashmit122@gmail.com <sashmit122@gmail.com>
2024-08-05 14:34:44 +0530

    empty commit

--- provider.tf
+++ provider.tf
@@ -1,13 +1,12 @@
 provider "snowflake" {
   role = "SYSADMIN"
-  private_key_path = "snowflake_tf_snow_key.p8"
 
 }
 
 provider "snowflake" {
   alias = "security_admin"
   role  = "SECURITYADMIN"
-  private_key_path = "snowflake_tf_snow_key.p8"
+
 
 }
 
